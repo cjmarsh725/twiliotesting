08f19b3175112e6a4205c938fb159a3753af5bce
Chris <cjmarsh725@gmail.com>
2019-03-21 14:55:07 -0700

    server implemented with stock sms message

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,30 @@
+const express = require("express");
+const cors = require('cors');
 require("dotenv").config();
 const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
+const server = express();
+server.use(express.json());
+server.use(cors({}));
+
+server.post('/', (req, res) => {
+  client.messages
+    .create({
+       body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+       from: '+14086179092',
+       to: '+14087187190'
+     })
+    .then(message => console.log(message.sid));
+  res.json({response: "message sent"});
+})
+
+server.listen(process.env.PORT, err => {
+  if (err) console.log(err);
+  else console.log(`Server listening on port ${process.env.PORT}`);
+})
+
+
+/*
 client.messages
   .create({
      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
@@ -8,3 +32,4 @@ client.messages
      to: '+14087187190'
    })
   .then(message => console.log(message.sid));
+  */
